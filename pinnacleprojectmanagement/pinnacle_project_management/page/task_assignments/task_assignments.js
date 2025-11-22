a9a1722cfa0b0674b01493555aeade5bade755f8
jyotiraditya1-mygstcafe <jyotiraditya@mygstcafe.in>
2025-11-22 17:49:27 +0530

    refactor: remove task assignments page and associated files
    Date: 22-11-2025
    By: Piyush Shukla

--- pinnacleprojectmanagement/pinnacle_project_management/page/task_assignments/task_assignments.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['task-assignments'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Task Assignment Dashboard',
-		single_column: true
-	});
-}
